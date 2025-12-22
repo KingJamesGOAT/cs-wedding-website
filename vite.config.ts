0a5df2237a9a0dd781bf0deaa5732f071b0f749d
KingJamesGOAT <sbstevebenjamin@gmail.com>
2025-12-22 20:14:38 +0100

    chore: prepare for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/sc-wedding-website/',
+    base: '/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
