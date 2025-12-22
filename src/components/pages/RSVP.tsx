e0812fe611bd0af1fa13d0879fe723b8bdda5f8c
KingJamesGOAT <sbstevebenjamin@gmail.com>
2025-12-22 14:38:43 +0100

    Switch RSVP submission to x-www-form-urlencoded

--- src/components/pages/RSVP.tsx
+++ src/components/pages/RSVP.tsx
@@ -38,9 +38,9 @@ export default function RSVP() {
         method: 'POST',
         mode: 'no-cors',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: JSON.stringify(submissionData),
+        body: new URLSearchParams(submissionData as any).toString(),
       });
 
       // Since mode is no-cors, we get an opaque response.
