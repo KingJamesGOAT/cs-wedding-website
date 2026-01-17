e29b760cdff824d7db46498e91d351addc32d0e0
KingJamesGOAT <sbstevebenjamin@gmail.com>
2026-01-17 17:18:43 +0100

    Remove check_images scripts

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,9 @@
 import { useRef, useState, useEffect } from 'react';
 import { useLanguage } from '../../contexts/LanguageContext';
 import CountdownTimer from '../CountdownTimer';
-import heroImageDesktop from '../../assets/hero-desktop-large-1.png';
-import heroImageV4 from '../../assets/hero-desktop-large-2.png';
-import heroImageMobile from '../../assets/hero-mobile-large.png';
+import heroImageDesktop from '../../assets/hero-image-v3.jpg';
+import heroImageV4 from '../../assets/hero-image-v4.jpg';
+import heroImageMobile from '../../assets/hero-image-mobile-v2.jpg';
 import { motion, useScroll, useTransform, AnimatePresence } from 'framer-motion';
 
 const desktopImages = [heroImageDesktop, heroImageV4];
